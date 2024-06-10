967bf09cd4b2b7075fd1756406354a7d7e3ca9ac
Rey <Miguel.Pena1709@gmail.com>
2024-06-10 18:59:29 -0400

    profile picture added

---
+++ script.js
@@ -0,0 +1,7 @@
+function hideWelcomeBox() {
+    document.getElementById('welcomeBox').style.display = 'none';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('welcomeBox').style.display = 'block';
+});
